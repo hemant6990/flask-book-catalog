c16e08e6d070f14ba14d151b433bef4e2155f71b
hemant6990 <hemantmehta69@gmail.com>
2020-05-14 13:59:58 +0530

    made new changes

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = True
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Stinson@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
